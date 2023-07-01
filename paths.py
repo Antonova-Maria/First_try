82e70932410d5554e7611becb61212fd1ff76bbf
Maria Antonova <mannya7@mail.ru>
2023-07-01 08:30:36 +0300

    reloaded file paths

--- paths.py
+++ paths.py
@@ -1,7 +1,13 @@
-#with open('C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers_Array_WO_Signs.txt', "w") as newopen:
-#    p = ['C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\masking.yaml', 'C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\no_objects.yaml', 'C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\road.yaml', 'C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\road_defects.yaml', 'C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\road_markup.yaml', 'C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\road_objects.yaml', 'C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\tech_mark.yaml', 'C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\vehicles.yaml']
-#    for i in p:
-#        with open(i, encoding="utf8") as o:
-#            for line in o:
-#                print(line, end="", file=newopen)
-#            print("\n", file=newopen)
+from pathlib import Path
+from datetime import datetime
+with open('C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers_Array_WO_Signs.txt', "w") as newopen:
+    d = datetime.today()               
+    print(d.strftime("%d/%m/%y %H:%M:%S"), file=newopen)
+    p = Path('C:\\Users\\manny\\OneDrive\\Рабочий стол\\Classifiers\\')
+    list_path = list(p.glob('*'))
+    for i in list_path:
+        with open(i, encoding="utf8") as o:
+            for line in o:
+                print(line, end="", file=newopen)
+            print("\n", file=newopen)
+print("Преобразование в один файл завершено")
