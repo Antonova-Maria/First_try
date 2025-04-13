6310c96e0dc7f07523165632c42b3793a4085e68
Antonova_Maria <126966610+Antonova-Maria@users.noreply.github.com>
2025-04-13 20:46:13 +0000

    +ограничение размера картинки

--- guess_cards.py
+++ guess_cards.py
@@ -9,12 +9,28 @@ st.set_page_config(layout="wide")
 image_path = 'cards/абрикос.png'
 image = Image.open(image_path)
 
+# Получаем оригинальные размеры
+width, height = image.size
+
+# Максимальная высота, которую вы хотите установить
+max_height = 400  # в пикселях
+
+# Если изображение выше максимальной высоты, изменяем его размер
+if height > max_height:
+    # Вычисляем новую ширину, сохраняя соотношение сторон
+    new_width = int(width * (max_height / height))
+    # Изменяем размер изображения
+    image = image.resize((new_width, max_height))
+
+# Отображаем изображение
+st.image(image, use_container_width=False)
+
 # Получаем название файла без расширения (это будет слово-ответ)
 image_name = os.path.basename(image_path).split('.')[0]
 word_length = len(image_name)
 
 # Отображение изображения по ширине контейнера
-st.image(image, use_container_width=True)
+#st.image(image, use_container_width=True)
 
 # Создаем колонки для полей ввода букв - по одной колонке на каждую букву
 letter_cols = st.columns(word_length)
